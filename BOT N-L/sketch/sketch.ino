c882a5ba3095bf492592c8df8e0d3adbf5d695b5
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2023-02-07 12:35:56 +0100

    Created file and added structure with comments

---
+++ BOT N-L/sketch/sketch.ino
@@ -0,0 +1,20 @@
+//=============[LIBRARIES]=============
+
+//=============[SET PINS]=============
+
+//=============[DECLARE VARIABLES]=============
+
+//=============[SETUP]=============
+void setup() {
+  // put your setup code here, to run once:
+
+}
+//=============[LOOP]=============
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+//=============[END OF PROGRAM]=============
+
+
+//=============[Functions]=============
