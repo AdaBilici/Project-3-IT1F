5a395fdce4ab1fccde50043da782d3a577693aba
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2023-02-16 15:55:55 +0100

    Stored basic movements in functions

--- BOT N-L/sketch/sketch.ino
+++ BOT N-L/sketch/sketch.ino
@@ -3,10 +3,14 @@
 #include <Servo.h> // https://github.com/arduino-libraries/Servo
 
 //=============[SET PINS]=============
-const int a1 = 11; //
-const int a2 = 10; //
-const int b1 = 9; //
-const int b2 = 6; // 
+const int leftBack = 11; // Left wheel back
+const int leftForward = 10; // Left wheel forward
+const int rightForward = 9; // Right wheel forward
+const int rightBack = 5; // Right wheel back
+
+const int PIN = 4;
+const int numbPixels = 4;
+
 const int firstButtonPin = 12; // B1 pin
 const int secondButtonPin = 8; // B2 pin
 const int gripper = 7; // Gripper pin
@@ -22,21 +26,59 @@ void setup() {
   servoMotor.attach(7);
   pinMode(firstButtonPin, INPUT);
   pinMode(secondButtonPin, INPUT);
-  pinMode(a1, OUTPUT);
-  pinMode(a2, OUTPUT);
-  pinMode(b1, OUTPUT);
-  pinMode(b2, OUTPUT);
+
+  Serial.begin(9600);
+  pinMode(leftBack, OUTPUT);
+  pinMode(leftForward, OUTPUT);
+  pinMode(rightForward, OUTPUT);
+  pinMode(rightBack, OUTPUT);
 }
 //=============[LOOP]=============
 void loop() {
-  analogWrite(a1, 255);
-  /*closeGripper();
-  openGripper();*/
+  moveBackwards();
+  
+  closeGripper();
+  openGripper();
 }
 
 /**************[END OF PROGRAM]**************/
 
 //=============[Functions]=============
+void moveForward() {
+  analogWrite(leftBack,0);
+  analogWrite(leftForward, 255);
+  analogWrite(rightForward, 255);
+  analogWrite(rightBack,0);
+}
+
+void moveBackwards() {
+  analogWrite(leftBack,255);
+  analogWrite(leftForward, 0);
+  analogWrite(rightForward, 0);
+  analogWrite(rightBack, 255);
+}
+
+void turnLeft() {
+  analogWrite(leftBack,0);
+  analogWrite(leftForward, 255);
+  analogWrite(rightForward, 0);
+  analogWrite(rightBack,0);
+}
+
+void turnRight() {
+  analogWrite(leftBack,0);
+  analogWrite(leftForward, 0);
+  analogWrite(rightForward, 255);
+  analogWrite(rightBack,0);
+}
+
+void rotateOnAxis() {
+  analogWrite(leftBack,0);
+  analogWrite(leftForward, 255);
+  analogWrite(rightForward, 0);
+  analogWrite(rightBack,255);
+}
+
 void closeGripper() { // Close gripper with button one
   buttonStateOne = digitalRead(firstButtonPin);
   if (buttonStateOne == LOW) {
@@ -47,6 +89,6 @@ void closeGripper() { // Close gripper with button one
 void openGripper() {
   buttonStateTwo = digitalRead(secondButtonPin);
   if (buttonStateTwo == LOW) { // Close gripper with button two
-    servoMotor.write(120);
+    servoMotor.write(100);
   }
 }
