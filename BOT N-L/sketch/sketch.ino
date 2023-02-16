8651a2a3ed93501aa8d186bd435a791e7d383fe6
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2023-02-16 15:08:07 +0100

    Created functions to open and close the gripper

--- BOT N-L/sketch/sketch.ino
+++ BOT N-L/sketch/sketch.ino
@@ -1,20 +1,52 @@
 //=============[LIBRARIES]=============
+#include <Adafruit_NeoPixel.h> // https://github.com/adafruit/Adafruit_NeoPixel
+#include <Servo.h> // https://github.com/arduino-libraries/Servo
 
 //=============[SET PINS]=============
+const int a1 = 11; //
+const int a2 = 10; //
+const int b1 = 9; //
+const int b2 = 6; // 
+const int firstButtonPin = 12; // B1 pin
+const int secondButtonPin = 8; // B2 pin
+const int gripper = 7; // Gripper pin
+Servo servoMotor; // Declaring servo object
 
 //=============[DECLARE VARIABLES]=============
+int pos = 0;
+int buttonStateOne = 0;
+int buttonStateTwo = 0;
 
 //=============[SETUP]=============
 void setup() {
-  // put your setup code here, to run once:
-
+  servoMotor.attach(7);
+  pinMode(firstButtonPin, INPUT);
+  pinMode(secondButtonPin, INPUT);
+  pinMode(a1, OUTPUT);
+  pinMode(a2, OUTPUT);
+  pinMode(b1, OUTPUT);
+  pinMode(b2, OUTPUT);
 }
 //=============[LOOP]=============
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  analogWrite(a1, 255);
+  /*closeGripper();
+  openGripper();*/
 }
-//=============[END OF PROGRAM]=============
 
+/**************[END OF PROGRAM]**************/
 
 //=============[Functions]=============
+void closeGripper() { // Close gripper with button one
+  buttonStateOne = digitalRead(firstButtonPin);
+  if (buttonStateOne == LOW) {
+    servoMotor.write(40);  
+  }
+}
+
+void openGripper() {
+  buttonStateTwo = digitalRead(secondButtonPin);
+  if (buttonStateTwo == LOW) { // Close gripper with button two
+    servoMotor.write(120);
+  }
+}
