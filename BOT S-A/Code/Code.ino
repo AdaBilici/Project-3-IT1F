2eba52416ba9046f8cb56437183d2e1ea985b4ac
Stefan Ionut Tasca <stefan.tasca@student.nhlstenden.com>
2023-03-08 11:25:03 +0100

    Alignment

--- BOT S-A/Code/Code.ino
+++ BOT S-A/Code/Code.ino
@@ -1,15 +1,25 @@
 #include <Adafruit_NeoPixel.h>
+#include <Servo.h>
+#include <Adafruit_NeoPixel.h>
 #include <SoftwareSerial.h>
-// the setup function runs once when you press reset or power the board
+#include <Servo.h>
 //int ledRed=13;
 //int ledYellow=12;
 //int ledGreen=11;
 //int brightness=0;
-//DistanceSenzor
 
-const int trigPin = 8;
-const int echoPin = 9;
+//===[ DistanceSenzors ]============================
 
+const int trigPinFront = 8;
+const int echoPinFront = 9;
+
+const int trigPinLeft = 2;
+const int echoPinLeft = 3;
+
+//===[ Gripper ]====================================
+
+const int gripper_pin=4;
+Servo gripper;
 //bluetooth
 
 long duration;
@@ -18,36 +28,40 @@ const int BT_R=5;
 const int BT_T=6;
 SoftwareSerial BT_module(BT_R,BT_T);
 
-//motor pins
-const int  MOTOR_A1=A0;
-const int MOTOR_A2=A1;  
-const int MOTOR_B1=A2;
-const int MOTOR_B2=A3;
+//===[ Button Pins + States ]======================
 
-//buttons pins
-
+/*
 const int BUTTON1=2;
 const int BUTTON2=3;
 const int BUTTON3=4;
 
-//button states;
-
 int buttonState1=0;
 int buttonState2=0;
 int buttonState3=0;
+*/
 
-//pixels
+//===[ Pixels ]====================================
 
 const int PIXEL_PIN=7;
 const int PIXEL_NUMBER=4;
 
-//colors
+//===[ Led Pixels ]================================
 Adafruit_NeoPixel leds(PIXEL_NUMBER, PIXEL_PIN, NEO_RGB + NEO_KHZ800);
 const uint32_t RED=leds.Color(255,0,0);
 const uint32_t YELLOW=leds.Color(255,150,0);
 const uint32_t BLUE=leds.Color(0,0,255);
 const uint32_t WHITE=leds.Color(255,255,255);
 const uint32_t START=leds.Color(0,0,0);
+
+//===[ Functions ]=================================
+
+void setup_motor_pins()
+{ 
+  pinMode(MOTOR_A1,OUTPUT);
+  pinMode(MOTOR_A2,OUTPUT);
+  pinMode(MOTOR_B1,OUTPUT);
+  pinMode(MOTOR_B2,OUTPUT);
+}
 
 void moveForward(int power=255)
 {
@@ -67,6 +81,8 @@ void moveBackwards(int power=255)
   analogWrite(MOTOR_B2,power);
 }
 
+//===[SETUP ]============================
+
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
@@ -77,13 +93,16 @@ void setup() {
   leds.show();
   setup_motor_pins();
   pinMode(BUTTON1,INPUT);
+  pinMode(BUTTON2,INPUT);
+//  gripper.attach(gripper_pin);
 }
 
-// the loop function runs over and over again forever
+int value=0;
+
+//===[ LOOP ]============================
 
 void loop() {
   //distance senzor
-  
     // Clears the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -97,13 +116,11 @@ void loop() {
   distance = duration * 0.034 / 2;
   // Prints the distance on the Serial Monitor
   Serial.print("Distance: ");
-  Serial.println(distance);
-  
-  //finish
-
+  Serial.println(distance);  
   
   buttonState1=digitalRead(BUTTON1);
   buttonState2=digitalRead(BUTTON2);
+
   if (buttonState1==LOW)
   {
   moveForward(255);
@@ -113,7 +130,8 @@ void loop() {
     analogWrite(MOTOR_A1,0);
     analogWrite(MOTOR_B1,0);
     }
-    
+
+
   if (buttonState2==LOW)
   {
     moveBackwards(255);
@@ -122,15 +140,20 @@ void loop() {
     analogWrite(MOTOR_A2,0);
     analogWrite(MOTOR_B2,0);
     }
-  leds.fill(START,0,4);
-  leds.show();
-}
-
-//Functions
-void setup_motor_pins()
-{
-  pinMode(MOTOR_A1,OUTPUT);
-  pinMode(MOTOR_A2,OUTPUT);
-  pinMode(MOTOR_B1,OUTPUT);
-  pinMode(MOTOR_B2,OUTPUT);
+    
+/*
+ if(buttonState1==LOW)
+ {value++;
+ gripper.write(160);
+ delay(5000);
+ }
+ 
+ if(value>0)
+ value --;
+ 
+ gripper.write(0);
+ */
+ 
+ leds.fill(START,0,4);
+ leds.show();
 }
