228acedd733d704b5328485173a532be094a60f8
Stefan Ionut Tasca <stefan.tasca@student.nhlstenden.com>
2023-03-15 10:11:05 +0100

    Interrupts + pulses complete

--- BOT S-A/Code/Code.ino
+++ BOT S-A/Code/Code.ino
@@ -5,10 +5,10 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
 
-//int ledRed=13;
-//int ledYellow=12;
-//int ledGreen=11;
-//int brightness=0;
+//===[ Time variables ]=============================
+
+int currentTime=0;
+int startTime=0;
 
 //===[ DistanceSenzors ]============================
 
@@ -19,8 +19,8 @@ const int echoPinFront = 9;
 
 long durationLeft;
 int distanceLeft;
-const int trigPinLeft = 2;
-const int echoPinLeft = 3;
+const int trigPinLeft = 5;
+const int echoPinLeft = 6;
 
 //===[ Gripper ]====================================
 
@@ -33,32 +33,27 @@ const int BT_R=5;
 const int BT_T=6;
 SoftwareSerial BT_module(BT_R,BT_T);
 
-//motor pins
-const int  MOTOR_A1=13;
-const int MOTOR_A2=12;  
-const int MOTOR_B1=11;
-const int MOTOR_B2=4;
+//===[ Motor pins ]======================
 
-//===[ Button Pins + States ]======================
+const int motorRightBackwards=13;
+const int motorRightForward=12;  
+const int motorLeftBackwards=11;
+const int motorLeftForward=10;
 
-/*
-const int BUTTON1=2;
-const int BUTTON2=3;
-const int BUTTON3=4;
+//===[ Motor tests for pulses ]====================
 
-int buttonState1=0;
-int buttonState2=0;
-int buttonState3=0;
-*/
-
-//===[ Pixels ]====================================
-
-const int PIXEL_PIN=7;
-const int PIXEL_NUMBER=4;
+const int motor_R1=2;
+const int motor_R2=3;
+int countLeft=0;
+int countRight=0;
+int countsLeft=0,previousCountLeft;
+int countsRight=0,previousCountRight;
 
 
 //===[ Led Pixels ]================================
 
+const int PIXEL_PIN=7;
+const int PIXEL_NUMBER=4;
 Adafruit_NeoPixel leds(PIXEL_NUMBER, PIXEL_PIN, NEO_RGB + NEO_KHZ800);
 const uint32_t RED=leds.Color(255,0,0);
 const uint32_t YELLOW=leds.Color(255,150,0);
@@ -76,26 +71,61 @@ const uint32_t START=leds.Color(0,0,0);
 
 void setup_motor_pins()
 { 
-  pinMode(MOTOR_A1,OUTPUT);
-  pinMode(MOTOR_A2,OUTPUT);
-  pinMode(MOTOR_B1,OUTPUT);
-  pinMode(MOTOR_B2,OUTPUT);
+  pinMode(motorRightBackwards,OUTPUT);
+  pinMode(motorRightForward,OUTPUT);
+  pinMode(motorLeftBackwards,OUTPUT);
+  pinMode(motorLeftForward,OUTPUT);
 }
 void moveForward(int power=255)
 {
-  digitalWrite(MOTOR_A1,power);
-  digitalWrite(MOTOR_B1,power);
+
+  digitalWrite(motorLeftForward,215);
+  digitalWrite(motorRightForward,power);
 }
 
 void moveBackwards(int power=255)
 {
-  digitalWrite(MOTOR_A2,power);
-  digitalWrite(MOTOR_B2,power);
+  digitalWrite(motorRightBackwards,power);
+  digitalWrite(motorLeftBackwards,power);
 }
 
+void stopRobot() {
+  analogWrite(motorRightBackwards,0);
+  analogWrite(motorRightForward, 0);
+  analogWrite(motorLeftBackwards, 0);
+  analogWrite(motorLeftForward,0);
+}
+
+void rotateOnAxis()
+{
+  analogWrite(motorLeftBackwards,0);
+  analogWrite(motorLeftForward, 200);
+  analogWrite(motorRightForward, 0);
+  analogWrite(motorRightBackwards,200);
+}
+
+void CountA()
+{
+  noInterrupts();
+  countLeft++;
+  Serial.println(countLeft);
+  interrupts();
+}
+
+void CountB()
+{
+  noInterrupts();
+  countRight++;
+  Serial.println(countRight);
+  interrupts();
+}
 //===[SETUP ]============================
 
 void setup() {
+  pinMode(motor_R1, INPUT_PULLUP);
+  pinMode(motor_R2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(motor_R1),CountA,CHANGE);
+  attachInterrupt(digitalPinToInterrupt(motor_R2),CountB,CHANGE);
   pinMode(trigPinFront, OUTPUT);
   pinMode(echoPinFront, INPUT);
   pinMode(trigPinLeft, OUTPUT); // Sets the trigPin as an Output
@@ -110,14 +140,12 @@ void setup() {
 //  gripper.attach(gripper_pin);
 }
 
-int value=0;
-
 //===[ LOOP ]============================
 
 void loop() {
   //distance 
   // Sets the trigPin on HIGH state for 10 micro secondssensor
- 
+  
   digitalWrite(trigPinLeft, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPinLeft, LOW);
@@ -134,20 +162,31 @@ void loop() {
   durationFront = pulseIn(echoPinFront, HIGH);
   // Calculating the distance
   distanceFront = durationFront * 0.034 / 2;
-
-
-if(distanceFront > 0 && distanceLeft < 10)
+  countLeft=0;
+  countRight=0;
+  
+  while(countLeft<80&&countRight<80)
+  {moveForward(255);
+   CountA();
+   CountB();
+  }
+  stopRobot();
+  delay(5000);
+/*
+if(distanceFront > 10 && distanceLeft < 10)
 {
  leds.fill(BLUE,0,4);
  leds.show();
- moveForward(255);
+ rotateOnAxis();
+ delay(250);
+ stopRobot();
 }
 else {
-  digitalWrite(MOTOR_A1,0);
-  digitalWrite(MOTOR_B1,0);
+ stopRobot();
     leds.fill(RED, 0, 4);
     leds.show();
   }
+*/
 }
 
 //Line sensor
